cd6cddfd61857f582f4f05988cf8ca9b005e67e1
ayasafushimi <bolbus0617@gmail.com>
2021-05-25 21:25:57 +0900

    やることが完了する機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   get '/articles', to: 'articles#new'
   post '/articles', to: 'articles#create'
+  delete '/articles/:id', to: 'articles#destroy', as: :complete
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
