d202f9ee7c4786948ccdc0fe6139eef8d90a6e77
ayasafushimi <bolbus0617@gmail.com>
2021-05-30 19:29:37 +0900

    [fix]検索機能をransackで実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,7 +22,9 @@ class ArticlesController < ApplicationController
   end
 
   def search
-    @articles = Article.text_like(params[:text]).datetime_between(params[:datetime_from], params[:datetime_to]).page(params[:page]).per(3)
+    # @articles = Article.text_like(params[:text]).datetime_between(params[:datetime_from], params[:datetime_to]).page(params[:page]).per(3)
+    @q = Article.ransack(params[:q])
+    @articles = @q.result.page(params[:page]).per(3)
   end
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Article.create(text: "牛乳を買う", datetime: '2021-07-21 17:00')
+Article.create(text: "髪を切る", datetime: '2021-06-13 13:00')
+Article.create(text: "役所に行く", datetime: '2021-06-21 10:00')
+Article.create(text: "ディナーを予約する", datetime: '2021-06-17 18:00')
+Article.create(text: "ハサミを買う", datetime: '2021-09-04 09:00')
+Article.create(text: "服を買う", datetime: '2021-06-13 10:00')
