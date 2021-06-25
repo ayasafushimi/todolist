a41a91731351ca3c4a65d5b665c418ce1111000b
ayasafushimi <bolbus0617@gmail.com>
2021-06-25 14:40:08 +0900

    システムテストをリファクタリング

--- spec/system/todos_spec.rb
+++ spec/system/todos_spec.rb
@@ -1,66 +1,59 @@
 require 'rails_helper'
 
 describe 'Todolist', type: :system do
-  let!(:todo_a) { FactoryBot.create(:todo, task: '最初のtodo', duedate: '202106171700') }
 
   describe '一覧表示機能' do
 
-    context '一覧表示画面へ遷移すると' do
-      before do
-        visit '/'
-      end
+    let!(:todo_a) { FactoryBot.create(:todo, task: '最初のtodo', duedate: '202106171700') }
 
-      it 'todoが表示される' do
-        expect(page).to  have_content '最初のtodo'
-      end
+    it '一覧表示画面にtodoが表示されること' do
+      visit '/'
+      expect(page).to  have_content '最初のtodo'
     end
   end
 
   describe '詳細表示機能' do
 
-    context 'todo詳細表示画面へ遷移すると' do
-      before do
-        visit todo_path(todo_a)
-      end
-      
-      it '詳細が表示される' do
-        expect(page).to  have_content '最初のtodo'
-      end
+    let!(:todo_a) { FactoryBot.create(:todo, task: '最初のtodo', duedate: '202106171700') }
+
+    it '詳細表示画面に詳細が表示されること' do
+      visit todo_path(todo_a)
+      expect(page).to  have_content '最初のtodo'
     end
   end
 
   describe '新規作成機能' do
-    
+
+    let(:valid_task) { '新規作成のテストを書く' }
+    let(:empty_task) { '' }
+    let(:valid_duedate) { '00202106171700' }
+    let(:empty_duedate) { '' }
+
     before do
       visit new_todo_path
-      fill_in 'Task',	with: task
-      fill_in 'Duedate',	with: duedate
-      click_button '登録'
     end
 
-    context '新規作成画面でtaskとduedateを入力したとき' do
-      let(:task) { '新規作成のテストを書く' }
-      let(:duedate) { '00202106171700' }
-
-      it '正常に登録される' do
+    context '有効な値が入力されたとき' do
+      it '正常に登録されること' do
+        fill_in 'Task',	with: valid_task
+        fill_in 'Duedate', with: valid_duedate
+        click_button '登録'
         expect(page).to  have_content '新しいtodoが作成されました'
       end
     end
 
-    context "新規作成画面でtaskを入力しなかったとき" do
-      let(:task) { '' }
-      let(:duedate) { '00202106171700' }
-
-      it 'エラーが発生する' do
+    context "無効な値が入力されたとき" do
+      it 'タスクに空文字が入力されるとエラーメッセージがでること' do
+        fill_in 'Task',	with: empty_task
+        fill_in 'Duedate', with: valid_duedate
+        click_button '登録'
         expect(page).to  have_content 'Taskを入力してください'
       end
-    end
-    
-    context "新規作成画面でduedateを入力しなかったとき" do
-      let(:task) { '新規作成のテストを書く' }
-      let(:duedate) { '' }
 
-      it 'エラーが発生する' do
+      it '期限に空文字が入力されるとエラーメッセージがでること' do
+        fill_in 'Task',	with: valid_task
+        fill_in 'Duedate', with: empty_duedate
+        click_button '登録'
         expect(page).to  have_content 'Duedateを入力してください'
       end
     end
@@ -68,70 +61,56 @@ describe 'Todolist', type: :system do
 
   describe '編集機能' do
 
-    before do
+    let!(:todo_a) { FactoryBot.create(:todo, task: '最初のtodo', duedate: '202106171700') }
+    let(:valid_task) { '編集機能のテストを書く' }
+
+    it 'todoを編集して更新できること' do
       visit todo_path(todo_a)
       click_button '編集'
-      fill_in 'Task', with: task
+      fill_in 'Task', with: valid_task
       click_button '更新'
+      expect(page).to  have_content 'todoが更新されました'
     end
 
-    context '編集画面でtaskを入力したとき' do
-      let(:task) { '編集機能のテストを書く' } 
 
-      it 'todoが更新される' do
-        expect(page).to  have_content 'todoが更新されました'
-      end
-    end
-    
   end
 
   describe '削除機能' do
 
-    before do
+    let!(:todo_a) { FactoryBot.create(:todo, task: '最初のtodo', duedate: '202106171700') }
+
+    it '削除ボタンを押すとtodoが削除されること' do
       visit todo_path(todo_a)
-    end
-
-    context '削除ボタンを押したとき' do
-      before do
-        accept_alert do
-          click_button '削除'
-        end
+      accept_alert do
+        click_button '削除'
       end
-
-      it 'todoが削除される' do
-        expect(page).to  have_content 'todoが削除されました'
-      end
+      expect(page).to  have_content 'todoが削除されました'
     end
   end
 
   describe '検索機能' do
 
+    let!(:todo_a) { FactoryBot.create(:todo, task: '最初のtodo', duedate: '202106171700') }
+
+    let(:task) { 'todo' }
+    let(:duedate_from) { '00202106010000' }
+    let(:duedate_to) { '00202107010000' }
+
     before do
       visit '/'
+    end
+
+    it 'タスクを入力して検索ができること' do
       fill_in 'やること', with: task
+      click_button '検索'
+      expect(page).to  have_content '最初のtodo'
+    end
+
+    it '期限を指定して検索ができること' do
       fill_in 'q[duedate_gteq]', with: duedate_from
       fill_in 'q[duedate_lteq]', with: duedate_to
       click_button '検索'
-    end
-    
-    context 'Taskを入力して検索すると' do
-      let(:task) { 'todo' }
-      let(:duedate_from) { '' }
-      let(:duedate_to) { '' } 
-
-      it '該当のtodoが表示される' do
-        expect(page).to  have_content '最初のtodo'
-      end
-    end
-
-    context 'Duedateを入力して検索すると' do
-      let(:task) { '' } 
-      let(:duedate_from) { '00202106010000' }
-      let(:duedate_to) { '00202107010000' }
-
-      it '該当のtodoが表示される' do
-        expect(page).to  have_content '最初のtodo'
-      end
+      expect(page).to  have_content '最初のtodo'
     end
   end
 
