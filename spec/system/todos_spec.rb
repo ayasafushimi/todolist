9a59d246ff43caf9a49061c4c77f49dad607b85f
ayasafushimi <bolbus0617@gmail.com>
2021-09-02 23:03:42 +0900

    [fix]todo新規作成できないエラーを修正

--- spec/system/todos_spec.rb
+++ spec/system/todos_spec.rb
@@ -77,7 +77,8 @@ describe Todo, type: :system do
       it '正常に登録されること' do
         fill_in 'Todo',	with: valid_task
         fill_in '締め切り日', with: valid_duedate
-        click_button '登録'
+        click_link_or_button '確認'
+        click_link_or_button '登録'
         # 登録完了のメッセージが表示されることを確認する
         expect(page).to  have_content '新しいTodoが作成されました'
       end
@@ -87,14 +88,14 @@ describe Todo, type: :system do
       it 'タスクに空文字が入力されるとエラーメッセージがでること' do
         fill_in 'Todo',	with: empty_task
         fill_in '締め切り日', with: valid_duedate
-        click_button '登録'
+        click_link_or_button '確認'
         expect(page).to  have_content "#{Todo.human_attribute_name(:task)}を入力してください"
       end
 
       it '期限に空文字が入力されるとエラーメッセージがでること' do
         fill_in 'Todo',	with: valid_task
         fill_in '締め切り日', with: empty_duedate
-        click_button '登録'
+        click_link_or_button '確認'
         expect(page).to  have_content "#{Todo.human_attribute_name(:duedate)}を入力してください"
       end
     end
