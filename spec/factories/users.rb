fc5145e6141b6118a384bb43cd1eafac8993c47e
ayasafushimi <bolbus0617@gmail.com>
2021-11-30 18:19:47 +0900

    userモデルスペックをリファクタリング

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,10 @@ FactoryBot.define do
     name { "taro" }
     email { "sample@test.com" }
     password_digest { "password" }
+
+    # Todo作成済みのユーザー
+    trait :with_todos do
+      after(:create) {|user| create_list(:todo, 5, user: user)}
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,46 +1,46 @@
 require 'rails_helper'
 
-describe User do
+RSpec.describe User, type: :model do
+  it '名前、メールアドレス、パスワードがあれば有効な状態であること' do
+    user = User.new
+    user.name = 'taro'
+    user.email = 'sample@test.com'
+    user.password_digest = 'password'
 
-  describe "#create" do
+    user.valid?
+    expect(user).to  be_valid
+  end
 
-    it '名前、メールアドレス、パスワードがあれば有効な状態であること' do
-      user = User.new
-      user.name = 'taro'
-      user.email = 'sample@test.com'
-      user.password_digest = 'password'
+  it '重複したメールアドレスは登録ができないこと' do
+    FactoryBot.create(:user, email: "sample@test.com")
 
+    user = FactoryBot.build(:user, email: "sample@test.com")
+    user.valid?
+    expect(user.errors[:email]).to  include('はすでに存在します')
+  end
+
+  context "値が未入力の場合" do
+    it '名前が未入力の場合は登録ができないこと' do
+      user = FactoryBot.build(:user, name: nil)
       user.valid?
-      expect(user).to  be_valid
+      expect(user.errors[:name]).to  include('を入力してください')
     end
 
-    it '重複したメールアドレスは登録ができないこと' do
-      FactoryBot.create(:user, email: "sample@test.com")
-
-      user = FactoryBot.build(:user, email: "sample@test.com")
+    it 'メールアドレスが未入力の場合は登録ができないこと' do
+      user = FactoryBot.build(:user, email: nil)
       user.valid?
-      expect(user.errors[:email]).to  include('はすでに存在します')
+      expect(user.errors[:email]).to  include('を入力してください')
     end
 
-    context "値が未入力の場合" do
-      it '名前が未入力の場合は登録ができないこと' do
-        user = FactoryBot.build(:user, name: nil)
-        user.valid?
-        expect(user.errors[:name]).to  include('を入力してください')
-      end
-
-      it 'メールアドレスが未入力の場合は登録ができないこと' do
-        user = FactoryBot.build(:user, email: nil)
-        user.valid?
-        expect(user.errors[:email]).to  include('を入力してください')
-      end
-
-      it 'パスワードが未入力の場合は登録ができないこと' do
-        user = FactoryBot.build(:user, password_digest: nil)
-        user.valid?
-        expect(user.errors[:password]).to  include('を入力してください')
-      end
+    it 'パスワードが未入力の場合は登録ができないこと' do
+      user = FactoryBot.build(:user, password_digest: nil)
+      user.valid?
+      expect(user.errors[:password]).to  include('を入力してください')
     end
+  end
 
+  it 'たくさんのtodoがついていること' do
+    user = FactoryBot.create(:user, :with_todos)
+    expect(user.todos.length).to eq 5
   end
 end
