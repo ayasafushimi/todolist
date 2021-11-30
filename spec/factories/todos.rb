e01b06648108c608c99f0f393fc5488ca8e21e03
ayasafushimi <bolbus0617@gmail.com>
2021-11-30 19:18:17 +0900

    todoモデルスペックへテストを追加

--- spec/factories/todos.rb
+++ spec/factories/todos.rb
@@ -2,5 +2,22 @@ FactoryBot.define do
   factory :todo do
     task {'テストを書く'}
     duedate {'202106171700'}
+    association :user
+
+    trait :done do
+      state { 'done' }
+    end
+
+    trait :todo do
+      state { 'todo' }
+    end
+
+    trait :task_length_50 do
+      task { 'ぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひ' }
+    end
+
+    trait :task_length_51 do
+      task { 'ぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひあ' }
+    end
   end
 end
--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,7 +1,18 @@
 require 'rails_helper'
 
-# RSpec.describe Todo, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Todo, type: :model do
+  it 'taskの長さが50文字であれば有効な状態であること' do
+    todo = FactoryBot.build(:todo, :task_length_50)
+
+    expect(todo).to be_valid
+  end
+
+  it 'taskの長さが51文字であれば無効な状態であること' do
+    todo = FactoryBot.build(:todo, :task_length_51)
+
+    todo.valid?
+    expect(todo.errors[:task]).to include('は50文字以内で入力してください')
+  end
+end
 
 
